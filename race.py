b88e2535a98da5bd6e414ee9cfb2df15ca1b7b90
Zachary Kurmas <kurmasz@gvsu.edu>
2023-10-25 09:48:57 -0400

    Initial commit

---
+++ race.py
@@ -0,0 +1,16 @@
+
+
+#
+# Instance variables
+#   name
+#   start_hours, start_minutes
+#   end_hours, end_minutes
+#   distance
+#   pace
+#
+# Instance methods
+#   elapsed_time
+#   pace
+#   __str__
+
+class Race
